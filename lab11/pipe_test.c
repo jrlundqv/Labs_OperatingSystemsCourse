62bbccc96e9f18b41cea7b0ddd378d3a4925deff
Jonathan <jrlundqv@hiof.no>
2022-11-03 13:20:44 +0100

    formatting

--- lab11/pipe_test.c
+++ lab11/pipe_test.c
@@ -14,33 +14,35 @@ int main(int argc, char** argv)
 		perror("Failed to create pipe\n");
 		return -1;
 	}
-	
+
 	pid_t child = fork();
-	if(child == 0){// This is a child process
+	if (child == 0) // This is a child process
+	{
 		close(my_pipe[READ_END_OF_FILE]);
 		FILE* out = fdopen(my_pipe[WRITE_END_OF_FILE], "w"); // open up a file, associate a stream with a file descriptor
-		sleep(2); 
+		sleep(2);
 		fprintf(out, "Are you my parent?"); // print something
 		return 42;
 	}
-	else 
-	if(child > 0){	// This is a parent process
-		
+	else if (child > 0)
+	{	// This is a parent process
+
 		close(my_pipe[WRITE_END_OF_FILE]);
 		FILE* in = fdopen(my_pipe[READ_END_OF_FILE], "r"); // open up as a file stream, associate a stream with a file descriptor
 		char buffer[100];
-		
+
 		fgets(buffer, 100, in);
 		printf("My child asked \"%s\"\n", buffer);
-		
+
 		int status;
 		wait(&status);
 		printf("And then returned %d\n", WEXITSTATUS(status));
-	 }
-	 else{
-		perror("Failed to fork\n");
+	}
+	else
+	{
+	perror("Failed to fork\n");
 		return -1;
-	  }
-	 return 0;
+	}
+	return 0;
 }
 
